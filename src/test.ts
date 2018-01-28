4c67fdaa3bb8f4e8d8449c6a4c3e30e7900e488c
Dariusz Zak <zakdariusz@gmail.com>
2018-01-28 23:44:01 +0100

    test: add some tests for sagas

--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,35 @@
 import { getStorySaga, API_URL } from './sagas'
-import { call } from 'redux-saga/effects'
-import { getStory } from './actions'
+import { call, put } from 'redux-saga/effects'
+import { getStory, getStorySucceeded, getStoryFailed } from './actions'
+import { Story } from './types'
 
 describe(`redux sagas`, () => {
   describe(`GetStory saga`, () => {
-    test(`should make request to hacker news api`, () => {
-      const storyId = 1
-      const it = getStorySaga(getStory(storyId))
-      expect(it.next().value).toEqual(call(fetch, `${API_URL}item/${storyId}.json`))
+    const prepareTest = () => ({
+      story: {
+        id: 1,
+        name: 'trolololo',
+      },
+      it: getStorySaga(getStory(1)),
+    })
+
+    test(`should dispatch GET_STORY_SUCCEEDED action when there is no error`, () => {
+      const { story, it } = prepareTest()
+      it.next() //call api
+      const val = it.next({ json: () => story }).value
+      expect(it.next(val).value).toEqual(put(getStorySucceeded(story)))
+    })
+
+    test(`should dispatch GET_STORY_FAILED if an error occurs`, () => {
+      const { story, it } = prepareTest()
+      it.next() //call api
+      const errorMsg = 'error while converting response to json'
+      const val = it.next({
+        json: () => {
+          throw Error(errorMsg)
+        },
+      }).value
+      expect(val).toEqual(put(getStoryFailed(errorMsg)))
     })
   })
 })
